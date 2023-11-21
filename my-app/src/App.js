0f851d23ab0e08a9ff94a78dc2e71dab969475a5
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-11-21 20:47:39 +0500

    New Game button & same numbers completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,21 @@
 import React from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
+  const [tanzies, setTanzies] = React.useState(false)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld === true)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTanzies(true)
+      console.log("WON")
+    }
+  }, [dice])
 
   function allNewDice() {
     const newDice = []
@@ -18,22 +30,35 @@ export default function App() {
   }
 
   function diceHold(id) {
-    console.log(id)
+    setDice((oldDice) => oldDice.map((die) => {
+      return die.id === id ? { ...die, isHeld: !die.isHeld } :
+        die
+    }))
   }
   const diceElements = dice.map((die) => {
     return <Die value={die.value} isHeld={die.isHeld} diceHold={() => diceHold(die.id)} />
   })
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice((oldDice) => oldDice.map((die) => {
+      return die.isHeld ? die : {
+        value: (Math.ceil(Math.random() * 6)),
+        isHeld: false,
+        id: nanoid()
+      }
+    }))
   }
 
   return (
     <main>
+      {tanzies && <Confetti />}
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="btn btn-primary" onClick={rollDice}>Roll</button>
+      <button className="btn btn-primary"
+        onClick={rollDice}>
+        {tanzies ? "NewGame" : "Roll"}
+      </button>
     </main>
   )
 }
