03219e9279a71c20a97403be657ceb6c22563e47
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-11-21 16:25:17 +0500

    hold-dice click function done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,39 @@
 import React from "react"
 import Die from "./Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      const randomNum = {
+        value: (Math.ceil(Math.random() * 6)),
+        isHeld: false,
+        id: nanoid()
+      }
+      newDice.push(randomNum)
+    } return newDice
+  }
+
+  function diceHold(id) {
+    console.log(id)
+  }
+  const diceElements = dice.map((die) => {
+    return <Die value={die.value} isHeld={die.isHeld} diceHold={() => diceHold(die.id)} />
+  })
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
   return (
     <main>
       <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
-
+        {diceElements}
       </div>
+      <button className="btn btn-primary" onClick={rollDice}>Roll</button>
     </main>
   )
 }
--- my-app/src/Die.js
+++ my-app/src/Die.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor : props.isHeld ? "green" : "white"
+    }
     return (
-        <div className="shadow">
+        <div className="shadow" 
+        style={styles}
+        onClick={props.diceHold}
+        >
             <h2>{props.value}</h2>
         </div>
     )
